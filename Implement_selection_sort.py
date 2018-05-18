8d7851e8852189bb5d065578f981b06b27b1c4b0
zyi103 <zyi103@syr.edu>
2018-05-18 10:44:27 +0800

    finished selection sorting 5-18-2018

---
+++ Implement_selection_sort.py
@@ -0,0 +1,26 @@
+def swap (array, firstIndex, secondIndex):
+	x = array[firstIndex] 
+	array[firstIndex] = array[secondIndex]
+	array[secondIndex] = x
+	
+def indexOfMin (array, startIndex):
+	minValue = array[startIndex]
+	minIndex = startIndex
+	
+	for i in array[startIndex:]:
+		if i < minValue:
+			minIndex = array.index(i)
+			minValue = i
+				
+	return minIndex
+	
+def selectionSort (array):
+	for idx, val in enumerate(array):
+		swap(array, idx, indexOfMin(array, idx))
+
+	
+array = [22, 11, 99, 88, 9, 7, 42]
+selectionSort(array)
+
+print("Array after sorting:  ", array)
+		
