17d5136c678e18f46fb84cf031070c9970f2ca09
zyi103 <zyi103@syr.edu>
2018-05-26 16:23:03 +0800

    recursive sorting 5-26-2018

---
+++ recursive_factorial.js
@@ -0,0 +1,15 @@
+var factorial = function(n) {
+	// base case: 
+	if ( n === 0 ){
+	    return 1;}
+	// recursive case:
+	    return n*factorial(n-1);
+}; 
+
+println("The value of 0! is " + factorial(0) + ".");
+println("The value of 5! is " + factorial(5) + ".");
+
+Program.assertEqual(factorial(0), 1);
+Program.assertEqual(factorial(5), 120);
+Program.assertEqual(factorial(2), 2);
+Program.assertEqual(factorial(1), 1);
