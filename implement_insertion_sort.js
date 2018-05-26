b81d53e36503acc74999595ba9c197d9ec9b4d21
zyi103 <zyi103@syr.edu>
2018-05-26 10:51:17 +0800

    Implement insertion sort 5-26-2018!

---
+++ implement_insertion_sort.js
@@ -0,0 +1,19 @@
+var insert = function(array, rightIndex, value) {
+    for(var j = rightIndex;
+        j >= 0 && array[j] > value;
+        j--) {
+        array[j + 1] = array[j];
+    }   
+    array[j + 1] = value; 
+};
+
+var insertionSort = function(array) {
+    for(var i = 1; i <= array.length -1; i++){
+        insert(array,i-1,array[i]);
+    }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+insertionSort(array);
+console.log("Array after sorting:  " + array);
+//Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
